e5542dd2af7e664f83721979b19299d4fd095789
Jagadeep06 <93898882+Jagadeep6@users.noreply.github.com>
2023-05-13 21:56:59 +0530

    DP
    Prblm 322

--- prblm322.cpp
+++ prblm322.cpp
@@ -1,13 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int coinChange(vector<int>& coins, int amount)
+    int coinChange(vector<int> &coins, int amount)
     {
-        if(amount == 0)
+        if (amount == 0)
         {
             return 0;
         }
-        
+        if (coins.size() == 0 and amount > 0)
+        {
+            return -1;
+        }
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= coins.size(); i++)
+        {
+            for (int j = 1; j <= amount; j++)
+            {
+                if (j >= coins[i - 1])
+                {
+                    dp[j] = min(dp[j], dp[j - coins[i - 1]] + 1);
+                }
+            }
+        }
+
+        if (dp[amount] == amount + 1)
+        {
+            return -1;
+        }
+        else
+        {
+            return dp[amount];
+        }
     }
 };
+
+int coinChange(vector<int> &coins, int amount)
+{
+    if (amount == 0)
+    {
+        return 0;
+    }
+    if (coins.size() == 0 and amount > 0)
+    {
+        return -1;
+    }
+    vector<int> dp(amount + 1, amount + 1);
+    dp[0] = 0;
+    for (int i = 1; i <= coins.size(); i++)
+    {
+        for (int j = 1; j <= amount; j++)
+        {
+            if (j >= coins[i - 1])
+            {
+                dp[j] = min(dp[j], dp[j - coins[i - 1]] + 1);
+            }
+        }
+    }
+
+    if (dp[amount] == amount + 1)
+    {
+        return -1;
+    }
+    else
+    {
+        return dp[amount];
+    }
+}
+
+int main()
+{
+    vector<int> coins = {1, 2, 5};
+    cout << coinChange(coins, 11) << endl;
+    return 0;
+}
